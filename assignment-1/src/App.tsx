aa11552ef98cfd4059ac7ec873516b2c6c95f4e4
sabihuddin10 <sabihuddin106@gmail.com>
2025-07-01 19:56:58 +0500

    feat: build quote generator with topic filtering and infinite scroll

---
+++ assignment-1/src/App.tsx
@@ -0,0 +1,12 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import QuoteGenerator from "./components/ui/QuoteGenerator"
+function App() {
+  return (
+    <>
+      <QuoteGenerator />
+    </>
+  )
+}
+
+export default App
